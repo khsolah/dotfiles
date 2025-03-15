e30a318e65882d109f4d352bdcb8b32e74d03387
khsolah <khsolah@gmail.com>
2025-03-15 15:07:42 +0800

    feat: add overseer autocmds example

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,32 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    print("auto callback")
+    local overseer = require("overseer")
+    local cwd = vim.fn.getcwd()
+
+    -- Define tasks based on the project directory
+    local auto_tasks = {
+      ["/"] = {
+        { name = "lint", cmd = "yarn lint" },
+      },
+    }
+
+    -- Check if there's a task for the current directory
+    local tasks = auto_tasks[cwd]
+    if tasks then
+      for _, task in ipairs(tasks) do
+        local new_task = overseer.new_task({
+          name = task.name,
+          cmd = task.cmd,
+          cwd = cwd,
+          components = { "default" },
+        })
+        new_task:start()
+      end
+    end
+  end,
+})
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.outline" },
-    -- { import = "lazyvim.plugins.extras.editor.overseer" },
+    { import = "lazyvim.plugins.extras.editor.overseer" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
